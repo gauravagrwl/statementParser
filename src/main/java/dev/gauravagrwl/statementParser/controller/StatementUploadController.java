e7dac0685d92f0ad2a5431f325b3117448a3f29f
Gaurav Agarwal <gauravagarwal@Gauravs-MacBook-Pro.local>
2021-01-30 01:32:58 -0600

    code refactor and fix todo

--- src/main/java/dev/gauravagrwl/statementParser/controller/StatementUploadController.java
+++ src/main/java/dev/gauravagrwl/statementParser/controller/StatementUploadController.java
@@ -1,10 +1,8 @@
 package dev.gauravagrwl.statementParser.controller;
 
 import java.io.BufferedInputStream;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
@@ -12,7 +10,10 @@ import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.AutoDetectParser;
 import org.apache.tika.sax.BodyContentHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,16 +23,26 @@ import org.springframework.web.multipart.MultipartFile;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 
-import dev.gauravagrwl.statementParser.controller.ChasePdfParser.ChaseRecord;
+import com.beust.jcommander.internal.Lists;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+
+import dev.gauravagrwl.statementParser.model.TransactionDetails;
+import dev.gauravagrwl.statementParser.parser.BankTypeOne;
 
 @RestController
 @RequestMapping("/uploadFile")
 public class StatementUploadController {
+	
+	@Autowired
+	private BankTypeOne statementParser;
 
-	@RequestMapping(path = "/singlefileupload/", method = RequestMethod.POST)
-	public ResponseEntity<List<ChaseRecord>> processFile(@RequestParam("file") MultipartFile file)
+	@RequestMapping(path = "/singlefileupload/", method = RequestMethod.POST, produces = MediaType.ALL_VALUE)
+	public ResponseEntity<String> processFile(@RequestParam("file") MultipartFile file)
 			throws IOException, SAXException, TikaException {
-		List<ChasePdfParser.ChaseRecord> full = new ArrayList<>();
 		InputStream inputStream =  new BufferedInputStream(file.getInputStream());
 		System.out.println("File Name: " + file.getOriginalFilename());
 		String year = StringUtils.left(file.getOriginalFilename(), 4);
@@ -40,15 +51,22 @@ public class StatementUploadController {
 		Metadata metadata = new Metadata();
 		pdfparser.parse(inputStream, contenthandler, metadata);
 		String data = contenthandler.toString();
-		List<ChasePdfParser.ChaseRecord> chaseRecords = ChasePdfParser.parse(data, year);
-		full.addAll(chaseRecords);
-		return (new ResponseEntity<List<ChasePdfParser.ChaseRecord>>(full, null, HttpStatus.OK));
+		TransactionDetails parse = statementParser.parse(data, year);
+		
+		ObjectMapper mapper = JsonMapper.builder()
+				   .addModule(new JavaTimeModule())
+				   .build();
+		
+		String jsonString = mapper.writeValueAsString(parse);
+		HttpHeaders headers = new HttpHeaders();
+	    headers.setContentType(MediaType.APPLICATION_JSON);
+		return new ResponseEntity<>(
+			      jsonString, headers, HttpStatus.OK);
 	}
 
-	@RequestMapping(path = "/multiplefileupload/", method = RequestMethod.POST)
-	public ResponseEntity<List<ChaseRecord>> processFile(@RequestParam("files") List<MultipartFile> files) throws IOException, SAXException, TikaException {
-		List<ChasePdfParser.ChaseRecord> full = new ArrayList<>();
-
+	@RequestMapping(path = "/multiplefileupload/", method = RequestMethod.POST, produces = MediaType.ALL_VALUE)
+	public ResponseEntity<String> processFile(@RequestParam("files") List<MultipartFile> files) throws IOException, SAXException, TikaException {
+		List<TransactionDetails> details = Lists.newArrayList();
 		for (MultipartFile file : files) {
 			System.out.println("File Name: " + file.getOriginalFilename());
 			String year = StringUtils.left(file.getOriginalFilename(), 4);
@@ -58,11 +76,18 @@ public class StatementUploadController {
 			Metadata metadata = new Metadata();
 			pdfparser.parse(inputStream, contenthandler, metadata);
 			String data = contenthandler.toString();
-			List<ChasePdfParser.ChaseRecord> chaseRecords = ChasePdfParser.parse(data, year);
-			full.addAll(chaseRecords);
-			
+			TransactionDetails parse = statementParser.parse(data, year);
+			details.add(parse);
 		}
-
-		return (new ResponseEntity<List<ChasePdfParser.ChaseRecord>>(full, null, HttpStatus.OK));
+		ObjectMapper mapper = JsonMapper.builder()
+				.addModule(new ParameterNamesModule())
+				   .addModule(new Jdk8Module())
+				   .addModule(new JavaTimeModule())
+				   .build();
+		String jsonString = mapper.writeValueAsString(details);
+		HttpHeaders headers = new HttpHeaders();
+	    headers.setContentType(MediaType.APPLICATION_JSON);
+		return new ResponseEntity<>(
+			      jsonString, headers, HttpStatus.OK);
 	}
 }
--- src/main/java/dev/gauravagrwl/statementParser/utility/statementParserConstants.java
+++ src/main/java/dev/gauravagrwl/statementParser/utility/statementParserConstants.java
@@ -2,19 +2,3 @@ package dev.gauravagrwl.statementParser.utility;
 
 public class statementParserConstants {
 }
-
-class chasePdfParser{
-    private chasePdfParser(){}
-}
-
-class pncPdfParser{
-    private pncPdfParser(){}
-}
-
-enum supportedFileType{
-
-}
-
-enum supportedDownloadType{
-
-}
